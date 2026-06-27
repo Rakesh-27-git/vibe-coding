03d159c3af2fdb6e4f263010f6d33b5c197d0434
Rakesh Choudhary <seervir987@gmail.com>
2026-06-27 13:40:54 +0530

    Add serverExternalPackages to Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  serverExternalPackages: ["@prisma/client", "@e2b/code-interpreter"],
 };
 
 export default nextConfig;
